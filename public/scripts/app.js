49c09a14d7ed82f4b8084e0477ee168048e27b5e
myrnz <okanbatuk@protonmail.com>
2022-10-24 01:33:02 +0300

    React Forms

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,33 +5,20 @@ var root = ReactDOM.createRoot(document.getElementById("root"));
 // React
 var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
   id: "header"
-}, "My First React App"), /*#__PURE__*/React.createElement("div", null, "Lorem, ipsum dolor."), /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum.")));
+}, "My First React App"), /*#__PURE__*/React.createElement("div", null, "Lorem, ipsum dolor."), /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum.")), /*#__PURE__*/React.createElement("form", {
+  onSubmit: onFormSubmit
+}, /*#__PURE__*/React.createElement("input", {
+  type: "text",
+  name: "txtItem"
+}), /*#__PURE__*/React.createElement("button", {
+  type: "submit"
+}, "Add Item")));
+var onFormSubmit = function onFormSubmit(event) {
+  var item = event.target.elements.txtItem.value;
+  alert("This ".concat(item, " has been added"));
+  console.log("This ".concat(item, " has been added"));
+  event.preventDefault();
+  // event.stopImmediatePropagation();
+};
 
-//? #region  Product Details
-var product = {
-  // model: "Xiaomi Note 8 PRO",
-  model: "",
-  price: 6000,
-  // price: "",
-  // desc: "iyi bir telefon",
-  desc: ""
-};
-var formatPrice = function formatPrice(p) {
-  return !p.price || p.price == "" ? p.price = 0 : p.price;
-};
-var getDesc = function getDesc(desc) {
-  return desc ? /*#__PURE__*/React.createElement("p", {
-    id: "product-desc"
-  }, "Description: ", desc) : /*#__PURE__*/React.createElement("p", {
-    id: "product-desc"
-  }, "Description: Product has no description");
-};
-var template2 = /*#__PURE__*/React.createElement("div", {
-  id: "product-details"
-}, "// if the product has a model information,", /*#__PURE__*/React.createElement("h2", {
-  id: "product-name"
-}, "name: ", product.model ? product.model : product.model = "no-name"), "// if the product has a price,", product.price && product.price != "" && product.price > 0 && /*#__PURE__*/React.createElement("p", {
-  id: "product-price"
-}, "Price = ", formatPrice(product), " TL "), getDesc(product.desc));
-
-//#endregion
+root.render(template);
--- src/app.js
+++ src/app.js
@@ -10,45 +10,19 @@ let template = (
       <li>Lorem, ipsum.</li>
       <li>Lorem, ipsum.</li>
     </ul>
+    <form onSubmit={onFormSubmit}>
+      <input type="text" name="txtItem" />
+      <button type="submit">Add Item</button>
+    </form>
   </div>
 );
 
-//? #region  Product Details
-let product = {
-  // model: "Xiaomi Note 8 PRO",
-  model: "",
-  price: 6000,
-  // price: "",
-  // desc: "iyi bir telefon",
-  desc: "",
+let onFormSubmit = (event) => {
+  let item = event.target.elements.txtItem.value;
+  alert(`This ${item} has been added`);
+  console.log(`This ${item} has been added`);
+  event.preventDefault();
+  // event.stopImmediatePropagation();
 };
 
-const formatPrice = (p) => {
-  return !p.price || p.price == "" ? (p.price = 0) : p.price;
-};
-
-const getDesc = (desc) => {
-  return desc ? (
-    <p id="product-desc">Description: {desc}</p>
-  ) : (
-    <p id="product-desc">Description: Product has no description</p>
-  );
-};
-
-let template2 = (
-  <div id="product-details">
-    // if the product has a model information,
-    <h2 id="product-name">
-      name: {product.model ? product.model : (product.model = "no-name")}
-    </h2>
-    // if the product has a price,
-    {product.price && product.price != "" && product.price > 0 && (
-      <p id="product-price">Price = {formatPrice(product)} TL </p>
-    )}
-    {getDesc(product.desc)}
-  </div>
-);
-
-//#endregion
-
-
+root.render(template);
