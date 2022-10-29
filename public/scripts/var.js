8c3091dbf3795ffbfd6e6f5f19a84e22dda64c8f
myrnz <okanbatuk@protonmail.com>
2022-10-30 01:19:45 +0300

    Bug Fixed

---
+++ public/scripts/var.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/*
+  ! Okan
+var nameVar = "Sadik";
+nameVar = "Osman";
+var nameVar = `Okan`;
+console.log(nameVar); 
+*/
+var nameLet = "Okan";
+var namelet = "Osman";
+console.log(nameLet);
---
+++ src/examples/var.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*
+  ! Okan
+var nameVar = "Sadik";
+nameVar = "Osman";
+var nameVar = `Okan`;
+console.log(nameVar); 
+*/
+
+let nameLet = "Okan";
+let namelet = "Osman";
+
+console.log(nameLet);
